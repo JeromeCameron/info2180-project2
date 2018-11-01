8fabf410aa50d283539401c793e9d443af442f0d
Jerome Cameron <jeromecameron@gmail.com>
2018-11-01 00:17:21 -0500

    added additional features

--- fifteen.js
+++ fifteen.js
@@ -1,4 +1,12 @@
 "use strict";
+/*
+Extra Features Used:
+1. Animations and/or transitions
+2. Multiple backgrounds
+3. Game time
+4. End-of-game notification
+*/
+
 window.onload = function(){
 
     //Global Variables
@@ -8,6 +16,8 @@ window.onload = function(){
     let Ypx = 0;        //Stores style.left px value
 
     //Apperance and Set Up
+        let backImages = ["background.jpg","abstract-art.jpg","art.jpg","city-skyline.jpg"];
+        let image = backImages[Math.floor(Math.random() * 4)];
 
         //On load this block of code sets up the appearance and attribute values of the puzzle pieces
         //Variables
@@ -19,12 +29,34 @@ window.onload = function(){
         const grid = 4;     //number of cloumns
         let count = 1;      //counter
         let count_2 = 1;    //counter
+
+        let node =  document.createElement("div");
+        parentTile.after(node);
+        node.style.margin="20px 20px 20px 20px";
+        node.innerHTML = "Select from  our other backgrounds<br>";
+
+        for(let i=0; i<4; i++){
+            let newNode = document.createElement("img");
+            node.after(newNode);
+            newNode.src = backImages[i];
+            newNode.style = "width:100px;height:100px;";
+            newNode.setAttribute("id", "image_"+i);
+
+            newNode.addEventListener("click", function(){
+                let pic = document.getElementById(newNode.id).src;
+                tiles.forEach(item => {
+                loadBackground(item,pic);
+                });
+            });
+        };
+
         //sets up tiles with background image
         tiles.forEach(item => {
+                let img = image;
                 item.classList.add('puzzlepiece');
                 item.style.top =(Math.floor(x / grid)) * 100 + "px";
                 item.style.left = ( x % grid ) * 100 + "px";
-                item.style.backgroundPosition = "-"+item.style.left+" "+"-"+item.style.top;
+                loadBackground(item,img);
                 //Variable counters
                 x += 1;
                 col += 1;
@@ -49,6 +81,7 @@ window.onload = function(){
             });
 
     //Behavior
+
 
     tiles.forEach(item => {
         item.addEventListener("click", function(){
@@ -75,15 +108,23 @@ window.onload = function(){
     });
 
     //This function moves puzzle pieces
-    function moveTile(item){
+    function moveTile(item,task){
         //Temp variables to track changing values
         let tempX = parseInt(item.style.top);
         let tempY = parseInt(item.style.left);
         let tempTileX = parseInt(item.id[5]);
         let tempTileY = parseInt(item.id[7]);
         //Assign new values to tile top and left style attributes to change its position
-        item.style.top = Xpx + 'px';
-        item.style.left = Ypx + 'px';
+        if(task === "shuffle"){
+            item.style.top = Xpx + 'px';
+            item.style.left = Ypx + 'px';
+        }else{
+            $(item).animate({
+                top: Xpx + 'px',
+                left: Ypx + 'px',
+            },500);
+        }
+
         Xpx = tempX;
         Ypx = tempY;
         item.setAttribute("id", "tile_" + blankTileX + "," + blankTileY);
@@ -96,15 +137,30 @@ window.onload = function(){
         let col = parseInt(item.id[7]);
 
         if((blankTileX - row === 1 && blankTileY - col === 0) || (blankTileX - row === 0 && blankTileY - col === 1)){
-            console.log("true");
             return true;
         }else if((blankTileX - row === 0 && blankTileY - col === -1) || (blankTileX - row === -1 && blankTileY - col === 0)){
             return true;
         }else{
-            console.log("false");
             return false;
         }
     }
 
-    /* Extra Features */
+    function shuffle(){
+        let task = "shuffle";
+        let button = document.querySelector("button");
+        button.addEventListener("click", function(){
+                    for(let i=0; i<100; i++){
+                        tiles.forEach(item => {
+                            if(isMovevable(item)){
+                            moveTile(item,task);
+                            }
+                        });
+                    }
+        })
+    }shuffle();
+
+    function loadBackground(item,img){
+        item.style.background = "url("+img+")";
+        item.style.backgroundPosition = "-"+item.style.left+" "+"-"+item.style.top;
+    }
 }
